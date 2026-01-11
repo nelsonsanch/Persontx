1d6d2c60b2c4034b8499536ce11f7da2c98bb5c7
Nelson <nelson@sanchezcya.com>
2026-01-10 21:57:19 -0500

    mejora inventarios

--- src/components/inventarios/configs/extintoresConfig.js
+++ src/components/inventarios/configs/extintoresConfig.js
@@ -4,24 +4,26 @@ export const extintoresConfig = {
     coleccion: 'inventarios',
     filtroCategoria: 'extintores', // Campo 'categoria' en Firebase
     campos: [
-        { name: 'codigo', label: 'Código/ID', type: 'text', required: true },
+        { name: 'codigo', label: 'Código/ID', type: 'text', required: true, showInTable: true },
         { name: 'foto', label: 'Fotografía', type: 'image', showInTable: true },
-        { name: 'ubicacion', label: 'Ubicación', type: 'text', required: true },
+        { name: 'ubicacion', label: 'Ubicación', type: 'text', required: true, showInTable: true },
         {
             name: 'tipoAgente',
             label: 'Tipo Agente',
             type: 'select',
             options: ['ABC (Polvo Químico)', 'CO2 (Dióxido Carbono)', 'Solkaflam', 'Agua', 'Espuma', 'Tipo K (Acetato de Aluminio)'],
-            required: true
+            required: true,
+            showInTable: true
         },
         {
             name: 'capacidad',
-            label: 'Capacidad (Lbs/Kg)',
+            label: 'Capacidad',
             type: 'select',
             options: ['2.5 Lbs', '5 Lbs', '10 Lbs', '20 Lbs', '30 Lbs', '40 Lbs', '150 Lbs (Satélite)'],
-            required: true
+            required: true,
+            showInTable: true
         },
-        { name: 'fechaCompra', label: 'Fecha de Compra/Adquisición', type: 'date', required: true },
+        { name: 'fechaCompra', label: 'Fecha de Compra', type: 'date', required: true, showInTable: true },
         { name: 'valor', label: 'Valor Comercial ($)', type: 'number', required: true },
         {
             name: 'componentes',
